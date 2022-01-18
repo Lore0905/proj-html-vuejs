b487d26b471d86d0c64aa9ea21ba18b7b076eb49
Lore0905 <lore.castelli4@gmail.com>
2022-01-18 09:41:53 +0100

    card components

---
+++ src/components/Card.vue
@@ -0,0 +1,60 @@
+<template>
+
+
+    <div class="container-card white">
+        <!-- logo -->
+        <div class="circle h-v-center">
+            <i class="fas fa-user-friends"></i>
+        </div>
+        <!-- title -->
+        <div class="title">
+            <h4>{{singleAdvantage.title}}</h4>
+        </div>
+        <!-- paragrafo -->
+        <div>
+            <p>{{singleAdvantage.paragrafo}}</p>
+        </div>
+    </div>
+
+
+</template>
+
+
+<script>
+export default {
+    name: "Card",
+    props: ['singleAdvantage']
+}
+</script>
+
+<style lang="scss">
+@import '../style/variables.scss';
+
+.container-card{
+    width: calc((100% / 3) - 20px);
+    padding: 40px 50px;
+    border-radius: 7px;
+    margin:10px;
+    .title{
+        padding: 20px 0;
+        h4{
+            font-weight: 500;
+        }
+    }
+    p{
+        color: $grey;
+        font-size: 18px;
+    }
+}
+
+// classi dinamiche
+.white{
+    background-color: white;
+}
+.circle{
+    border-radius: 50%;
+    width: 50px;
+    height: 50px;
+    background-color: $color3;
+}
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -4,20 +4,83 @@
         <div class="jumbotron">
             <Jumbotron/>
         </div>
+        <!-- card -->
+        <section class="advatages">
+            <div class="container">
+                <div class="wrapping-card">
+                    <Card v-for="(advantage, index) in advantages" :key="index" :singleAdvantage="advantage"/>
+                </div>
+            </div>
+        </section>
+        <!-- company section -->
+        <section class="company-section">
+            <div class="raw">
+                <div class="col-6">
+                    ciaqo
+                </div>
+                <div class="col-6">
+                    ciao
+                </div>
+            </div>
+        </section>
+        
     </main>
 </template>
 
 <script>
 import Jumbotron from './Jumbotron.vue'
+import Card from './Card.vue'
 export default {
     name: 'Main',
     components: {
-        Jumbotron
+        Jumbotron,
+        Card
+    },
+    data: function(){
+        return{
+            advantages: [
+                {
+                    title: 'Human Capital',
+                    paragrafo: 'Humanizing business: Harness the power of technology to improve the way people work.',
+                },
+                {
+                    title: 'Core Business',
+                    paragrafo: 'It takes innovative approaches to transform, and run existing platforms.'
+                },
+                {
+                    title: 'Perforamnce',
+                    paragrafo: 'Archieving maximum impact and value from investements in finance and supply chain.'
+                }
+            ]
+        }
     }
 }
 </script>
 
 <style lang="scss">
+@import '../style/variables.scss';
+main{
+    .advatages{
+        background-color: $color3;
+        width: 100%;
+        .container{
+            .wrapping-card{
+                display: flex;
+                flex-wrap: wrap;
+                padding: 60px 0;
 
+            }
+        }
+    }
+    .company-section{
+        .raw{
+            display: flex;
+            .col-6{
+                // test
+                border: 1px solid black;
+            }
+        }
+    }
+}
 
 </style>
